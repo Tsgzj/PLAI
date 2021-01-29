65961fe6dc8a7c1c4f03ae46c5959d12c1bbc62d
Tsgzj <wxsun1991@gmail.com>
2021-01-28 19:58:13 -0800

    Chap 4

--- chap2.rkt
+++ chap2.rkt
@@ -23,3 +23,21 @@
 (test (interp (multC
                (plusC (numC 2) (numC 3))
                (multC (numC 1) (numC 2)))) 10)
+
+(define-type ArithS
+  [numS (n : number)]
+  [plusS (l : ArithS) (r : ArithS)]
+  [bminusS (l : ArithS) (r : ArithS)]
+  [multS (l : ArithS) (r : ArithS)])
+
+(define (desugar [as : ArithS]) : ArithC
+  (type-case ArithS as
+    [numS (n) (numC n)]
+    [plusS (l r) (plusC (desugar l)
+                        (desugar r))]
+    [multS (l r) (multC (desugar l)
+                        (desugar r))]
+    [bminusS (l r) (plusC (desugar l)
+                          (multC (numC -1) (desugar r)))]))
+
+(test (interp (desugar (bminusS (numS 2) (numS 1)))) 1)
