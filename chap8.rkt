3c4fc4214f7702ca3dfb7432f6c0d0bdff151e1f
Tsgzj <wxsun1991@gmail.com>
2021-02-15 22:13:40 -0800

    Chap 8

--- chap8.rkt
+++ chap8.rkt
@@ -11,3 +11,25 @@
   [unboxC (arg : ExprC)]
   [setboxC (b : ExprC) (v : ExprC)]
   [seqC (b1 : ExprC) (b2 : ExprC)])
+
+(define-type Value
+  [numV (n : number)]
+  [closV (arg : symbol) (body : ExprC) (env : Env)]
+  [boxV (v : Value)])
+
+(define (interp [expr : ExprC] [env : Env]) : Value
+  (type-case ExprC expr
+             [numC (n) (numV n)]
+             [idC (n) (lookup n env)]
+             [appC (f a) (local ([define f-value (interp f env)])
+                           (interp (closV-body f-value)
+                                   (extend-env (bind (closV-arg f-value)
+                                                     (interp a env))
+                                               (closV-env f-value))))]
+             [plusC (l r) (num+ (interp l env) (interp r env))]
+             [multC (l r) (num* (interp l env) (interp r env))]
+             [lamC (a b) (closV a b env)]
+             [boxC (a) (boxV (interp a env))]
+             [unboxC (a) (boxV-V (interp a env))]
+             <setboxC-case>
+             <seqC-case>))
